8be591f02234b8075f68b794c953a95ddbe6f9f3
WbBuilder56 <saadanwaransari20@gmail.com>
2025-12-28 14:18:40 +0500

    updated the port

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,7 @@ app.post(
     }
   }
 );
-app.listen(5000, () => {
-  console.log("Backend running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
